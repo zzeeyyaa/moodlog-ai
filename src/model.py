b141fcb51da46da7b0a4c02c57f239c9bbc94e67
Milda Fauziah <fauziah.milda@gmail.com>
2025-05-31 21:50:33 +0700

    data dummy lebih stabil distribusinya, bertujuan meningkatkan akurasi

--- src/model.py
+++ src/model.py
@@ -2,7 +2,10 @@ from sklearn.ensemble import RandomForestClassifier
 
 def build_model():
     return RandomForestClassifier(
-        n_estimators=100,
-        max_depth=None,
+        n_estimators=200,
+        max_depth=10,
+        min_samples_split=5,
+        min_samples_leaf=2,
+        max_features='sqrt',
         random_state=42
     )
--- src/train.py
+++ src/train.py
@@ -1,13 +1,13 @@
 from src.data_loader import load_and_preprocess
 from src.model import build_model
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import accuracy_score, classification_report
 import joblib
 import os
 
 def train_and_save_model():
     # tryna load data and encode
-    X, y, le_aktivitas, le_kafein = load_and_preprocess()
+    X, y,le_bergadang, le_aktivitas, le_kafein = load_and_preprocess()
     
     # split data to data train and data test
     X_train, X_test, y_train, y_test =  train_test_split(X, y, test_size=0.2, random_state=42)
@@ -19,11 +19,14 @@ def train_and_save_model():
     # evaluate
     y_pred = model.predict(X_test)
     acc = accuracy_score(y_test, y_pred)
+    report = classification_report(y_test, y_pred)
+    print(f"Report klasifikasi: {report}")
     print(f"Akurasi model: {acc:.2f}")
     
     # save model and encoders
     os.makedirs("model", exist_ok=True)
     joblib.dump(model, "model/mood_model.pkl")
+    joblib.dump(le_bergadang, "model/le_bergadang.pkl")
     joblib.dump(le_aktivitas, "model/le_aktivitas.pkl")
     joblib.dump(le_kafein, "model/le_kafein.pkl")
     print("Model dan encoder disimpan di folder `model/`")
